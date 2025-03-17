bf39a48e771edc8502707ae2763d0d813f36ddae
Carlos LLanos <cllanoscor@gmail.com>
2025-03-17 17:42:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  //declarar constantes
+const nombre = ["Carlos", "Andres", "David"];
+const action = ["estudia", "enseña", "suspende"];
+const what = ["programación", "arquitectura", "contabilidad"];
+
+// indices random
+const randomName = nombre[Math.floor(Math.random() * nombre.length)];
+const randomAction = action[Math.floor(Math.random() * action.length)];
+const randomWhat = what[Math.floor(Math.random() * what.length)];
+
+console.log(`${randomName} ${randomAction} ${randomWhat}`);
   console.log("Hello Rigo from the console!");
 };
